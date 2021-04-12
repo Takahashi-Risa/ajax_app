5d4b0b7d8dae5547ebdd1860191d226dfb5ce9a9
Takahashi-Risa <nakajima.beans@gmail.com>
2021-04-13 03:25:01 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,18 @@
 class PostsController < ApplicationController
 
-def index
-  @posts = Post.order(id: "DESC")
-end
+  def index
+    @posts = Post.order(id: "DESC")
+  end
 
- # コメントアウトする
- # def new
- # end
+  # コメントアウトする
+  # def new
+  # end
 
-def create
-  Post.create(content: params[:content])
-  redirect_to action: :index  # 追記する
-end
+  def create
+    binding.pry
+    post = Post.create(content: params[:content])
+    # redirect_to action: :index  # 追記 #renderメソッドでJSONを指定に書き換え
+    render json:{ post: post }
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # get 'posts', to: 'posts#index'
   # get 'posts/new', to: 'posts#new'↓変更
-  root to: 'posts#index'
+  root to: 'posts#index'  
   post 'posts', to: 'posts#create'
 end
