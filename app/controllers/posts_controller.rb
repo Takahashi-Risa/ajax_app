9e1657d50c27aa58777537f6ffafae172259ad77
Takahashi-Risa <nakajima.beans@gmail.com>
2021-04-13 04:17:51 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
   # end
 
   def create
-    binding.pry
+    # binding.pry
     post = Post.create(content: params[:content])
     # redirect_to action: :index  # 追記 #renderメソッドでJSONを指定に書き換え
     render json:{ post: post }
--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html
+}
+
 function post (){
   // リクエストを送信する処理
   // console.log("イベント発火");
@@ -5,12 +19,36 @@ function post (){
   submit.addEventListener("click", (e) => {
     e.preventDefault();
     // console.log("イベント発火");
-    const form = document.getElementById("form");
+    const form = document.getElementById("form")
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+      // console.log(XHR.response);
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null; //JavaScriptの処理から抜け出す
+      }
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      // console.log(formText.value);
+            // ここから
+            // const item = XHR.response.post;
+            // const html = `
+            //   <div class = "post">
+            //     <div class="post-date">
+            //       投稿日時：${item.created_at}
+            //     </div>
+            //     <div class="post-content">
+            //       ${item.content}
+            //     </div>
+            //   </div> `;
+            // ↑buildHTMLとして外に切り出す
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
 }
 
